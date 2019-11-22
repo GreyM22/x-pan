0e4a088e34ce12261584a8c9b549ae9c2a55d08b
GreyM22 <greimuka@gmail.com>
2019-11-22 13:03:27 +0100

    changed

--- js/index.js
+++ js/index.js
@@ -32,6 +32,16 @@ $(function () {
 		}
 
 		return data.join("&");
+
+		/**********************************************************************/
+
+		//for the email-form 
+		$('#someTextBox').keypress(function (event) {
+			var keycode = (event.keyCode ? event.keyCode : event.which);
+			if (keycode == '13') {
+				alert('You pressed a "enter" key in textbox');
+			}
+		});
 	}
 
 
@@ -61,7 +71,6 @@ $(function () {
 
 		let x = parseInt(month);
 		let y = parseInt(currentDate.getMonth());
-		console.log(currentDate)
 
 		if (parseInt(month) > parseInt(currentDate.getMonth()) + 1) {
 			e.preventDefault();
@@ -75,7 +84,7 @@ $(function () {
 			request.open(formEmail.method, formEmail.action);
 			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			request.send(getFormDataString(formEmail));
-			$('.sms-form').text('Reservation made successfully')
+			$('#reserve-form .sms-form').text('Reservation made successfully')
 			$('  form').trigger("reset");
 		}
 		else if (parseInt(month) == parseInt(currentDate.getMonth() + 1) && parseInt(day) >= parseInt(currentDate.getDate())) {
@@ -84,17 +93,64 @@ $(function () {
 				request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 				request.send(getFormDataString(formEmail));
 				$(' form ').trigger("reset");
-				$(' .sms-form').text('Reservation made successfully')
+				$(' #reserve-form .sms-form').text('Reservation made successfully')
 			}
 			else {
-				$(' .sms-form').text('*Pleas book two hours in advance');
+				$(' #reserve-form .sms-form').text('*Pleas book two hours in advance');
 			}
 		}
 		else {
-			$(' .sms-form').text('*You can not reserve a table in the past. Please change your reservation date.');
+			$(' #reserve-form .sms-form').text('*You can not reserve a table in the past. Please change your reservation date.');
 		}
 
 	});
+	/**********************************************************************/
+
+	var formEmail = document.getElementById("email-form");
+
+	// Override the submit event
+
+	$(formEmail).keypress(function (e) {
+		var keycode = (e.keyCode ? e.keyCode : e.which);
+		if (keycode == '13') {
+			e.preventDefault();
+
+			let request = new XMLHttpRequest();
+
+			request.addEventListener("load", function () {
+				if (request.status === 302) { // CloudCannon redirects on success
+					console.log("worked")
+				}
+			});
+
+			request.open(formEmail.method, formEmail.action);
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			request.send(getFormDataString(formEmail));
+			$('.sms-subscription').fadeToggle('fast');
+			$('#email-form').trigger("reset");
+		}
+	});
+
+	// formEmail.addEventListener("submit", function (e) {
+
+	// 	console.log("function called ok")
+	// 	e.preventDefault();
+
+	// 	let request = new XMLHttpRequest();
+
+	// 	request.addEventListener("load", function () {
+	// 		if (request.status === 302) { // CloudCannon redirects on success
+	// 			console.log("worked")
+	// 		}
+	// 	});
+
+	// 	request.open(formEmail.method, formEmail.action);
+	// 	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	// 	request.send(getFormDataString(formEmail));
+	// 	$('.sms-subscription').fadeToggle('fast');
+	// 	$('#email-form').trigger("reset");
+	// });
+
 
 
 })
@@ -103,43 +159,43 @@ $(function () {
 var date = document.getElementById('date');
 
 function checkValue(str, max) {
-    if (str.charAt(0) !== '0' || str == '00') {
-        var num = parseInt(str);
-        if (isNaN(num) || num <= 0 || num > max) num = 1;
-        str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
-    };
-    return str;
+	if (str.charAt(0) !== '0' || str == '00') {
+		var num = parseInt(str);
+		if (isNaN(num) || num <= 0 || num > max) num = 1;
+		str = num > parseInt(max.toString().charAt(0)) && num.toString().length == 1 ? '0' + num : num.toString();
+	};
+	return str;
 };
 
 date.addEventListener('input', function (e) {
-    this.type = 'text';
-    var input = this.value;
-    if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
-    var values = input.split('/').map(function (v) {
-        return v.replace(/\D/g, '')
-    });
-    if (values[0]) values[0] = checkValue(values[0], 12);
-    if (values[1]) values[1] = checkValue(values[1], 31);
-    var output = values.map(function (v, i) {
-        return v.length == 2 && i < 1 ? v + ' / ' : v;
-    });
-    this.value = output.join('').substr(0, 14);
+	this.type = 'text';
+	var input = this.value;
+	if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
+	var values = input.split('/').map(function (v) {
+		return v.replace(/\D/g, '')
+	});
+	if (values[0]) values[0] = checkValue(values[0], 12);
+	if (values[1]) values[1] = checkValue(values[1], 31);
+	var output = values.map(function (v, i) {
+		return v.length == 2 && i < 1 ? v + ' / ' : v;
+	});
+	this.value = output.join('').substr(0, 14);
 });
 
 /* to format the time input */
 var time = document.getElementById('time');
 
 time.addEventListener('input', function (e) {
-    this.type = 'text';
-    var input = this.value;
-    if (/\D\:$/.test(input)) input = input.substr(0, input.length - 3);
-    var values = input.split(':').map(function (v) {
-        return v.replace(/\D/g, '')
-    });
-    if (values[0]) values[0] = checkValue(values[0], 23);
-    if (values[1]) values[1] = checkValue(values[1], 60);
-    var output = values.map(function (v, i) {
-        return v.length == 2 && i < 1 ? v + ' : ' : v;
-    });
-    this.value = output.join('').substr(0, 14);
+	this.type = 'text';
+	var input = this.value;
+	if (/\D\:$/.test(input)) input = input.substr(0, input.length - 3);
+	var values = input.split(':').map(function (v) {
+		return v.replace(/\D/g, '')
+	});
+	if (values[0]) values[0] = checkValue(values[0], 23);
+	if (values[1]) values[1] = checkValue(values[1], 60);
+	var output = values.map(function (v, i) {
+		return v.length == 2 && i < 1 ? v + ' : ' : v;
+	});
+	this.value = output.join('').substr(0, 14);
 });
