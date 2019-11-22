58ef17a07cb389d5ab4103d81e96606c184b05c9
GreyM22 <greimuka@gmail.com>
2019-11-22 14:48:42 +0100

    changed

--- js/index.js
+++ js/index.js
@@ -105,7 +105,7 @@ $(function () {
 		var keycode = (e.keyCode ? e.keyCode : e.which);
 		if (keycode == '13') {
 
-			$('#email-form').validate();
+			let  y = $('#email-form').validate();
 
 			if(!$('#email-form').validate()){
 				$('.sms-subscription').text('Enter a valid email address');
